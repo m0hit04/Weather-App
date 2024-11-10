3c245784c434a4778ab039d762785d05b2941eac
m0hit04 <msrathore0481@gmail.com>
2024-11-10 13:55:56 +0530

    Fetch data when click on refresh

--- src/components/Refresh.jsx
+++ src/components/Refresh.jsx
@@ -1,7 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { setCityName } from '../store/reducers/weatherSlice';
+
 export function Refresh() {
+    const dispatch = useDispatch();
+    const currentCity = useSelector(state => state.weather.cityName);
+
+    const handleRefresh = () => {
+        if (currentCity) {
+            dispatch(setCityName(currentCity));
+        }
+    };
+
     return (
         <>
-            <button className="mr-5 text-xl font-mono text-slate-500 dark:text-slate-200 hover:text-slate-700 dark:hover:text-white transition-colors duration-300">
+            <button 
+                onClick={handleRefresh}
+                className="mr-5 text-xl font-mono text-slate-500 dark:text-slate-200 hover:text-slate-700 dark:hover:text-white transition-colors duration-300"
+            >
                 Refresh
             </button>
         </>
